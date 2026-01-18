3d11b47daf210482b66d788bf348b918c596ed50
varshitha v <varshitha.vraj@winds.co.in>
2026-01-18 12:57:32 +0530

    adding project

---
+++ Emoji-Art/Emoji-Art/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Emoji-Art
+//
+//  Created by Varshitha VRaj on 18/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Emoji-Art/Emoji-Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji-Art
+//
+//  Created by Varshitha VRaj on 18/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
